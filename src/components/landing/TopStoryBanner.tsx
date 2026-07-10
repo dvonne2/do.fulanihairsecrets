9564052480971464bb473618fb88892eb4f3cdfb
dvonne2 <fulanihairgro2020@gmail.com>
2026-07-10 16:41:10 +0100

    Replace mama4.webp with bukky-2.png in customer reviews

--- src/components/landing/TopStoryBanner.tsx
+++ src/components/landing/TopStoryBanner.tsx
@@ -692,7 +692,7 @@ export const TopStoryBanner = () => {
                           `${BASE_PATH}assets/bukky-4.png`,
                           `${BASE_PATH}assets/bukky-1.png`,
                           `${BASE_PATH}assets/bukky-3.png`,
-                          `${BASE_PATH}assets/mama4.webp`,
+                          `${BASE_PATH}assets/bukky-2.png`,
                           `${BASE_PATH}assets/mama5.webp`,
                         ].map((src, index) => (
                           <img
